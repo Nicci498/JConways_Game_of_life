724a7fdce6301201ea40e5e05784a2c2bf042c36
Nicole white <you@example.com>
2020-05-28 07:42:13 -1000

    mvppppppppp

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # JConways_Game_of_life
 Memorial Project for John Conway's Game Of Life
 
-### Cellular Automata
+### Cellular Automata, Turing Completeness, & The Game of Life
 
-### Turing Completeness & The Game of Life
+A cellular automaton is a discrete model studied in automata theory. Cellular automata are also called cellular spaces, tessellation automata, homogeneous structures, cellular structures, tessellation structures, and iterative arrays.s
+
+In the 1980s, Stephen Wolfram engaged in a systematic study of one-dimensional cellular automata, or what he calls elementary cellular automata; his research assistant Matthew Cook showed that one of these rules is Turing-complete. Wolfram published A New Kind of Science in 2002, claiming that cellular automata have applications in many fields of science. These include computer processors and cryptography.
+
+ The primary classifications of cellular automata, as outlined by Wolfram, are numbered one to four. They are, in order, automata in which patterns generally stabilize into homogeneity, automata in which patterns evolve into mostly stable or oscillating structures, automata in which patterns evolve in a seemingly chaotic fashion, and automata in which patterns become extremely complex and may last for a long time, with stable local structures. This last class are thought to be computationally universal, or capable of simulating a Turing machine. 
+
+> https://en.wikipedia.org/wiki/Cellular_automaton
+
 
 It is possible to build a pattern that acts like a finite-state machine connected to two counters. This has the same computational power as a universal Turing machine, so the Game of Life is theoretically as powerful as any computer with unlimited memory and no time constraints; it is Turing complete
 
@@ -11,5 +18,7 @@ It is possible to build a pattern that acts like a finite-state machine connecte
 
 #### React
 #### Immer
+https://github.com/immerjs/immer
+
 Immer works by writing producers, the produce function takes two arguments, currentState and a producer function. Current state determines the starting point, and the producer tells us what needs to happen to it. The producer function receives one argument, the draft, which is a proxy to the current state you passed in. Any modification you make to the draft will be recorded and used to produce nextState, currentState will be untouched during this process. With Immer, we only need to reason about the changes we want to make relatively to the current state. Without needing to take the effort to actually produce the next state.
 > https://hackernoon.com/introducing-immer-immutability-the-easy-way-9d73d8f71cb3
--- life/src/Grid.js
+++ life/src/Grid.js
@@ -1,9 +1,12 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import produce from 'immer';
+var _ = require('lodash');
 
+const initialSize = { 
+  numRows: 30,
+  numCols: 50,
+}
 
-const numRows = 30;
-const numCols = 50;
 const operations = [
   [0, 1],
   [0, -1],
@@ -14,20 +17,30 @@ const operations = [
   [1, 0],
   [-1, 0],
 ];
-function initialState() {
-  const rows = [];
-  for (let i = 0; i < numRows; i++) {
-    rows.push(Array.from(Array(numCols), () => 0));
-  }
-  return rows;
-}
+
 function Grid() {
-  const [grid, setGrid] = useState(initialState);
+
+  const [size, setSize] = useState(initialSize);
   const [running, setrunning] = useState(false);
   const [generation, setGeneration] = useState(0);
   const [sum, setSum] = useState(0);
   const runningRef = useRef(running);
   runningRef.current = running;
+  const timeRef = useRef(300);
+  const numRows = 25;
+  const numCols = 70;
+  const colors = ["#B39BC8", "#F172A1", "#CB2D6F"]
+
+  function initialState() {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  }
+
+  const [grid, setGrid] = useState(initialState);
+  
   const runSim = useCallback(() => {
     if (!runningRef.current) {
       return;
@@ -35,11 +48,11 @@ function Grid() {
     setGrid((g) => {
       let validGrid = false;
       validGrid = false;
-      return produce(g, (gridCopy) => {
+      return produce(g, (gridCopy) => { // g is og, we have have the copy, which we change, and the copy makes changes to our og, 'g'
         for (let i = 0; i < numRows; i++) {
           for (let k = 0; k < numCols; k++) {
             let neighbors = 0;
-            operations.forEach(([x, y]) => {
+            operations.forEach(([x, y]) => { //looking for live/dead neighbors in every direction
               const newI = i + x;
               const newK = k + y;
               if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
@@ -47,7 +60,6 @@ function Grid() {
               }
             });
             if (neighbors < 2 || neighbors > 3) {
-              validGrid = true;
               gridCopy[i][k] = 0;
             } else if (g[i][k] === 0 && neighbors === 3) {
               validGrid = true;
@@ -65,8 +77,9 @@ function Grid() {
         )
       });
     });
-    setTimeout(runSim, 300);
-  }, []);
+    setTimeout(runSim, timeRef.current);
+  }, [timeRef.current]);
+  console.log(timeRef.current)
   return (
     <div className = 'container'>
         <div className = 'buttons'>
@@ -107,6 +120,20 @@ function Grid() {
       <div className = 'spacer'>
 
       </div>
+      <div className = 'buttons'>
+        <button 
+        onClick={() =>{
+          timeRef.current = 5
+        }}>Go Fast</button>
+        <button 
+        onClick={() =>{
+          timeRef.current = 500
+        }}>Go Normal</button>
+        <button 
+        onClick={() =>{
+          timeRef.current = 300
+        }}>Go Slow</button>
+      </div>
       <div className = 'grid'
         style={{
           display: 'grid',
@@ -126,7 +153,16 @@ function Grid() {
               style={{ //live cells
                 width: 14,
                 height: 15,
-                backgroundColor: grid[i][k] ? '#CB2D6F' : undefined,
+                backgroundColor:
+                grid[i][k] && sum > 100
+                  ? _.sample(colors)
+                  : grid[i][k] && sum > 75
+                  ? '#CB2D6F'
+                  : grid[i][k] && sum > 50
+                  ? '#F172A1'
+                  : grid[i][k] && sum >= 0
+                  ? '#B39BC8'
+                  : undefined,
                 border: 'solid 1.5px #501F3A',
               }}
             />
